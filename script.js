a07c4adf47224ba612bef5f14f710df03298f790
Tanisha <iamtanishadutta@gmail.com>
2025-08-03 09:58:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scroll to "features" section on button click
+document.getElementById("cta-button").addEventListener("click", function () {
+  document.getElementById("features").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
