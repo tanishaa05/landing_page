bec6dc9ab4ca61bcc8c1cf5080a8140fda2a539c
Tanisha <iamtanishadutta@gmail.com>
2025-08-03 09:56:43 +0530

    Delete Python directory

--- Python/factorial.py
+++
@@ -1,5 +0,0 @@
-s=1
-n=int(input("enter any number:"))
-for i in range(1,n+1):
-    s=s*i
-print("factorial is : ",s)
--- Python/table.py
+++
@@ -1,4 +0,0 @@
-n=int(input("enter a number : "))
-for i in range(1,11):
-    s=i*n
-    print("%d*%d=%d"%(n,i,s))
